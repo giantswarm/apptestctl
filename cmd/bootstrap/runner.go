98119c5a5cd6ab78f37d6e2fb3826fa5f4baed4a
Ross Fairbanks <ross@rossfairbanks.com>
2020-09-14 16:55:00 +0200

    Install app-operator and chart-operator

--- cmd/bootstrap/runner.go
+++ cmd/bootstrap/runner.go
@@ -7,11 +7,14 @@ import (
 	"time"
 
 	"github.com/giantswarm/apiextensions/v2/pkg/crd"
+	"github.com/giantswarm/appcatalog"
 	"github.com/giantswarm/backoff"
+	"github.com/giantswarm/helmclient/v2/pkg/helmclient"
 	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
 	"github.com/giantswarm/k8sclient/v4/pkg/k8srestconfig"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
@@ -19,6 +22,13 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
+)
+
+const (
+	appOperatorVersion                = "2.2.0"
+	chartOperatorVersion              = "2.3.1"
+	controlPlaneTestCatalogStorageURL = "https://giantswarm.github.io/control-plane-catalog/"
+	namespace                         = "giantswarm"
 )
 
 type runner struct {
@@ -84,6 +94,11 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 	}
 
 	err = r.ensureNamespace(ctx, k8sClients)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	err = r.installOperators(ctx, helmClient)
 	if err != nil {
 		return microerror.Mask(err)
 	}
@@ -182,6 +197,80 @@ func (r *runner) ensurePriorityClass(ctx context.Context, k8sClients k8sclient.I
 		return microerror.Mask(err)
 	} else {
 		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("created priorityclass %#q", priorityClassName))
+	}
+
+	return nil
+}
+
+func (r *runner) installOperators(ctx context.Context, helmClient helmclient.Interface) error {
+	var err error
+
+	operators := map[string]string{
+		"app-operator":   appOperatorVersion,
+		"chart-operator": chartOperatorVersion,
+	}
+
+	for name, version := range operators {
+		err = r.installOperator(ctx, helmClient, name, version)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	return nil
+}
+
+func (r *runner) installOperator(ctx context.Context, helmClient helmclient.Interface, name, version string) error {
+	var err error
+
+	var operatorTarballPath string
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("getting tarball URL for %#q", name))
+
+		operatorTarballURL, err := appcatalog.GetLatestChart(ctx, controlPlaneTestCatalogStorageURL, name, version)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("tarball URL is %#q", operatorTarballURL))
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "pulling tarball")
+
+		operatorTarballPath, err = helmClient.PullChartTarball(ctx, operatorTarballURL)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("tarball path is %#q", operatorTarballPath))
+	}
+
+	{
+		defer func() {
+			fs := afero.NewOsFs()
+			err := fs.Remove(operatorTarballPath)
+			if err != nil {
+				r.logger.LogCtx(ctx, "level", "error", "message", fmt.Sprintf("deletion of %#q failed", operatorTarballPath), "stack", fmt.Sprintf("%#v", err))
+			}
+		}()
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("installing %#q", name))
+
+		opts := helmclient.InstallOptions{
+			ReleaseName: fmt.Sprintf("%s-unique", name),
+		}
+
+		err = helmClient.InstallReleaseFromTarball(ctx,
+			operatorTarballPath,
+			namespace,
+			nil,
+			opts)
+		if helmclient.IsReleaseAlreadyExists(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("%#q already installed", name))
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("installed %#q", name))
 	}
 
 	return nil
