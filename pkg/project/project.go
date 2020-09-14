e477ef3e2ab3916b9dff78af4f1f4a2b79059288
Ross Fairbanks <ross@rossfairbanks.com>
2020-09-14 10:11:41 +0200

    Add version command

---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description = "Command line tool for using app platform in integration tests."
+	gitSHA      = "n/a"
+	name        = "apptestctl"
+	source      = "https://github.com/giantswarm/apptestctl"
+	version     = "n/a"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
