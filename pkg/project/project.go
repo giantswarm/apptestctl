0ea38015914c367ab5d55866d011b384ec24d5da
GitHub Action <action@github.com>
2020-10-30 16:48:01 +0000

    Bump version to 0.4.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "apptestctl"
 	source      = "https://github.com/giantswarm/apptestctl"
-	version     = "0.4.0"
+	version     = "0.4.1-dev"
 )
 
 func Description() string {
