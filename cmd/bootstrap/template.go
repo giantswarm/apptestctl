b1bc1f7386fffe3b4a9d68c1aede7433fa470fa6
Ross Fairbanks <ross@rossfairbanks.com>
2021-08-09 15:38:20 +0200

    Update to app-operator v5.1.1 and chart-operator v2.18.1

--- cmd/bootstrap/template.go
+++ cmd/bootstrap/template.go
@@ -17,15 +17,6 @@ probes:
 	operatorValuesYAML string = `operatorkit:
   resyncPeriod: "20s"
 
-Installation:
-  V1:
-    Helm:
-      HTTP:
-        ClientTimeout: "30s"
-      Kubernetes:
-        WaitTimeout: "180s"
-    Registry:
-      Domain: "quay.io"
-    Provider:
-      Kind: aws`
+provider:
+  kind: "aws"`
 )
