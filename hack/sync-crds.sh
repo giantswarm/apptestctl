b62dbea345114b8ef9c4fe0eef28076bd5d3e980
kubasobon <wtty.fool@gmail.com>
2023-04-27 12:58:58 +0200

    update hack/ & Makefile to sync Cilium CRDs

--- hack/sync-crds.sh
+++ hack/sync-crds.sh
@@ -3,5 +3,11 @@
 crds=( appcatalogentries appcatalogs apps catalogs charts )
 
 for crd in "${crds[@]}"; do
-        curl -s "https://raw.githubusercontent.com/giantswarm/apiextensions-application/master/config/crd/application.giantswarm.io_${crd}.yaml" > "../pkg/crds/${crd}.yaml"
+    curl -s "https://raw.githubusercontent.com/giantswarm/apiextensions-application/master/config/crd/application.giantswarm.io_${crd}.yaml" > "../pkg/crds/${crd}.yaml"
+done
+
+crds=( ciliumnetworkpolicies ciliumclusterwidenetworkpolicies )
+
+for crd in "${crds[@]}"; do
+    curl -s "https://raw.githubusercontent.com/cilium/cilium/main/pkg/k8s/apis/cilium.io/client/crds/v2/${crd}.yaml" > "../pkg/crds/${crd}.yaml"
 done
