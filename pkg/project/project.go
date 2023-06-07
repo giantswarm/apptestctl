feb2240e1ff6f28dccb0d0d6c21c7725aea7990e
taylorbot <dev@giantswarm.io>
2023-06-07 12:04:26 +0000

    Bump version to 0.17.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "apptestctl"
 	source      = "https://github.com/giantswarm/apptestctl"
-	version     = "0.17.0"
+	version     = "0.17.1-dev"
 )
 
 func Description() string {
