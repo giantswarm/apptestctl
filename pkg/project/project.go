79d71baeac2595fdf3d4c8bf4b7860e0fb9ed48a
taylorbot <dev@giantswarm.io>
2023-10-09 09:12:17 +0000

    Bump version to 0.18.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "apptestctl"
 	source      = "https://github.com/giantswarm/apptestctl"
-	version     = "0.18.0"
+	version     = "0.18.1-dev"
 )
 
 func Description() string {
